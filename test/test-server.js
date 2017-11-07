2ca4c832a41f697a4af56b963ba89506474e8c06
Ger Carton <gercarton.code@gmail.com>
2017-11-07 13:27:28 +0000

    inital commit

--- test/test-server.js
+++ test/test-server.js
@@ -137,4 +137,83 @@ describe('Shopping List', function() {
         res.should.have.status(204);
       });
   });
+
+  describe('recipes', function() {
+    before(function(){
+      return runServer();
+    }); 
+
+    after(function(){
+      return closeServer();
+    });
+
+  it('should list items on GET', function() {
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res){
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('array');
+      res.body.should.be.length.at.least(1);
+      const expectedKeys = ['name','ingredients'];
+      res.body.forEach(function(item) {
+        item.should.be.a('object');
+        item.should.include.keys(expectedKeys);
+    });  
+  });
 });
+
+  it('should add an item on POST', function() {
+    const newRecipe = {name:'stew', ingredients:['meat','potato']};
+    return chai.request(app)
+    .post('/recipes')
+    .send(newRecipe)
+    .then(function(res){
+      res.should.have.status(201);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.include.keys('name','ingredients');
+      res.body.id.should.not.be.null;
+      res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
+  });
+});
+
+    it('should delete items on DELETE', function() {
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/recipes/${res.body[0].id}`);
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });
+
+    it('should update items on PUT', function(){
+      const updateData={
+        name:'pie',
+        ingredients:['apple','dough','sugar']
+      };
+      return chai.request(app)
+      // first have to get so we have an idea of object to update
+      .get('/recipes')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+        // this will return a promise whose value will be the response
+        // object, which we can inspect in the next `then` back. Note
+        // that we could have used a nested callback here instead of
+        // returning a promise and chaining with `then`, but we find
+        // this approach cleaner and easier to read and reason about.
+        return chai.request(app)
+          .put(`/recipes/${updateData.id}`)
+          .send(updateData);
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });
+
+    })
+
+    }
